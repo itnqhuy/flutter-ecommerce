d7bc705984c1b832f415a679bfa3f6400e041e45
itnqhuy <huyahhin2019@gmail.com>
2025-04-25 13:12:28 +0700

    add on boarding screen

---
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class OnboardingController extends GetxController {
+  static OnboardingController get instance => Get.find();
+
+  void updatePageIndicator(index) {}
+
+  void dotNavigation(index) {}
+
+  void nextPage() {}
+
+  void skipPage() {}
+}
