029481ae6dbd63c4488edd98c3160237d6ba910b
itnqhuy <huyahhin2019@gmail.com>
2025-05-05 23:19:39 +0700

    add controller for login

---
+++ lib/features/authentication/controllers/login/login.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  static LoginController get instance => Get.find();
+
+  final pageController = PageController();
+}
