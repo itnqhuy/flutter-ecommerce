7acf4767f92c19a59656a03976753f802582e5a0
itnqhuy <huyahhin2019@gmail.com>
2025-05-07 00:13:40 +0700

    fix controller for authentication

--- lib/features/authentication/controllers/login/login.dart
+++ lib/features/authentication/controllers/login/login.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../shop/screens/home/home.dart';
 
 class LoginController extends GetxController {
   static LoginController get instance => Get.find();
 
   final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+
+  void updatePageIndicator(index) => currentPageIndex.value = index;
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpToPage(index);
+  }
+
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      Get.offAll(const HomeScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  void skipPage() {
+    Get.offAll(const HomeScreen());
+  }
 }
